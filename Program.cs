079d47dcf160a87ab9f7839b00a2fccc77e6a0aa
Jennifer D'Agostino <54606848+JennAnnDag7@users.noreply.github.com>
2020-02-05 11:17:53 -0500

    add solution

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LiftoffLiveCoding
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            double answer_one = Calculate_candy(.1, 0);
+            double answer_two = Calculate_candy(.25, -.05);
+            double answer_three = Calculate_candy(-.25, .2);
+
+            Console.WriteLine(answer_one);
+            Console.WriteLine(answer_two);
+            Console.WriteLine(answer_three);
+
+        }
+        public static double Calculate_candy(double x, double y)
+        {
+            double candytotal = (250 * 3) * (x + y) + (250 * 3);
+            return candytotal;
+
+        }
+    }
+}
